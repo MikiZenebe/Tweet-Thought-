a08ff661a6630467500c6215c4af88e0589c5aa9
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-03-25 21:04:35 +0300

    updated

--- components/Message.jsx
+++ components/Message.jsx
@@ -6,17 +6,20 @@ export default function Message({
   timestamp,
 }) {
   const date = timestamp.toDate().toLocaleTimeString("en-US");
-  console.log(date);
+  const time = timestamp.toDate().toDateString();
+  console.log(time);
   return (
-    <div className="bg-[#1B2730] max-w-[350px] mx-auto h-[120px]  rounded-lg mt-10 p-3 flex flex-col">
+    <div className="bg-[#1B2730] max-w-[350px] mx-auto h-[150px]  rounded-lg mt-10 p-3 flex flex-col">
       <div className="flex gap-2 items-center text-gray-300">
         <img src={avatar} alt="" className="w-8 h-8 rounded-full" />
-        <h2>{username}</h2>
+        <h2 className="text-green-300">{username}</h2>
       </div>
 
       <div className=" mx-10">
-        <p className="text-[12px] text-gray-400 mt-[-5px]">{date}</p>
-        <p className="mt-2">{description}</p>
+        <p className="text-[12px] text-gray-400 mt-[-5px]">
+          {time} {date}
+        </p>
+        <p className="mt-2 ">{description}</p>
       </div>
       {children}
     </div>
--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,30 +1,87 @@
-import { auth } from "../lib/firebase";
+import { auth, db } from "../lib/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import Message from "@/components/Message";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
 function Dashboard() {
   const route = useRouter();
   const [user, loading] = useAuthState(auth);
+  const [allPosts, setAllPosts] = useState([]);
 
   //Protect the page
   useEffect(() => {
     const getData = async () => {
       if (loading) return;
       if (!user) return route.push("/auth/login");
+
+      //Display posts if the user is found
+      const collRef = collection(db, "posts");
+      const q = query(collRef, where("user", "==", user.uid));
+      const unSub = onSnapshot(q, (snapshot) => {
+        setAllPosts(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      });
+      return unSub;
     };
 
     getData();
   }, [user, loading]);
 
+  //Delete Post
+  const deletePost = async (id) => {
+    const docRef = doc(db, "posts", id);
+    await deleteDoc(docRef);
+  };
+
   return (
     <div>
-      <div className="max-w-[400px] mx-auto">
-        <div className="bg-[#1B2730]">
-          <h1>Dashboard</h1>
-          <div>Posts</div>
-          <button onClick={() => auth.signOut()}>Sign out</button>
+      <div className="max-w-[400px] mx-auto py-4 h-[100vh]">
+        <h2 className="text-center text-2xl font-semibold">Your Posts</h2>
+
+        <div>
+          {allPosts.map((post) => {
+            return (
+              <Message key={post.id} {...post}>
+                <div className="flex mt-5 gap-4 mx-10">
+                  <button className=" text-[13px] flex items-center gap-2 bg-[#3f4a53] px-3 py-1 rounded-md text-blue-300">
+                    <span>
+                      <AiFillEdit />
+                    </span>
+                    Edit
+                  </button>
+
+                  <button
+                    onClick={() => deletePost(post.id)}
+                    className="text-[13px] flex items-center gap-2 bg-[#3f4a53] px-3 py-1 rounded-md text-red-300"
+                  >
+                    <span>
+                      <AiFillDelete />
+                    </span>
+                    Delete
+                  </button>
+                </div>
+              </Message>
+            );
+          })}
         </div>
+
+        <button
+          className="my-6 mx-6 px-2 py-1 rounded-lg bg-gradient-to-br from-blue-500 to-cyan-700 text-gray-200"
+          onClick={() => auth.signOut()}
+        >
+          Sign out
+        </button>
       </div>
     </div>
   );
